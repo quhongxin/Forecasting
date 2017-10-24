4fc18eff2d8a9b773632d368c6ee4e06c6e09aea
quhongxin <834030748@qq.com>
2017-10-24 10:42:27 +0800

    commit

--- www/js/common/global.js
+++ www/js/common/global.js
@@ -1,7 +1,7 @@
 var Global = {
 	Token: 'eyJ1c2VyOElEIjoiODAwMDAwNjAiLCJleHAiOjE1MDg5MDgyOTEsIlBhc3N3b3JkIjoiWHh4In0=',
 	Domain: '',
-	UserID: '501401',
-	User8ID: '80000060',
+	UserID: '',
+	User8ID: '',
 	LanguageCode: 'en',
 }
