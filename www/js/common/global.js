45d8600891de8f963b5649c27b7dd284a542e0a0
quhongxin <834030748@qq.com>
2017-10-27 13:01:10 +0800

    commit

--- www/js/common/global.js
+++ www/js/common/global.js
@@ -1,7 +1,7 @@
 var Global = {
 	Token: 'eyJ1c2VyOElEIjoiODAwMDAwNjAiLCJleHAiOjE1MDg5MDgyOTEsIlBhc3N3b3JkIjoiWHh4In0=',
-	Domain: '',
-	UserID: '',
-	User8ID: '',
+	Domain: 'https://cndev.dhc.com.cn',
+	UserID: '501401',
+	User8ID: '80000060',
 	LanguageCode: 'en',
 }
